1fd9205108a6cc1de7df98e6e7cbae0b0a3e964d
Pablor29 <pablorios961@gmail.com>
2024-08-17 13:01:01 -0300

    Initial commit

---
+++ TEST/script.js
@@ -0,0 +1,17 @@
+// script.js
+const fechaEvento = new Date('2024-12-31T23:59:59'); // Cambia la fecha y hora del evento
+
+function actualizarContador() {
+    const ahora = new Date();
+    const tiempoRestante = fechaEvento - ahora;
+
+    const segundos = Math.floor(tiempoRestante / 1000) % 60;
+    const minutos = Math.floor(tiempoRestante / 1000 / 60) % 60;
+    const horas = Math.floor(tiempoRestante / 1000 / 60 / 60) % 24;
+    const dias = Math.floor(tiempoRestante / 1000 / 60 / 60 / 24);
+
+    document.getElementById('reloj').textContent = `${dias} días ${horas}h ${minutos}m ${segundos}s`;
+}
+
+actualizarContador();
+setInterval(actualizarContador, 1000);
